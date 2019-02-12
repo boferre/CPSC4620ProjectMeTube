725af927e1336b2bbdecaf2ee1f9e51c61ed7c95
Brandon Orion Ferrell <boferre@webapp.cs.clemson.edu>
2019-02-12 16:18:10 -0500

    V0.1 - Extremely basic formatting and css created

--- index.php
+++ index.php
@@ -1,11 +1,44 @@
-<link rel="stylesheet" type="text/css" href="css/default.css" />
-<?php
-session_start();
+<head>
+	<link rel="stylesheet" type="text/css" href="css/main.css" />
+</head>
 
+<?php
+	session_start();
 ?>
 
-<html>
-	<body>
-		Testing 
-	</body>
+
+<html id="webPage">
+	<header id="headCont">
+		<table>
+			<tr id="headData">
+				<td> <span id="logoCont">logo</span> <input type="text" name="searchVal"><button type="button">Search</button>  <span id="menuCont">Upload Register</span> </td>
+			</tr>
+		</table>		
+	</header>
+
+	<div id="mainPageContent">
+		<table>
+			<tr id="spotlightedVid">
+				<td>
+					spotlighted Media
+				</td>
+			</tr>
+			
+			<tr id="mediaPrev">
+				<td>
+					Media preview section
+				</td>
+			</tr>
+
+			<tr id="mediaPrev">
+				<td>
+					Media Preview Section
+				</td>
+			</tr>
+		</table>
+	</div>
+
+	<footer id="footCont">
+		other links and information
+	</footer>
 </html>
