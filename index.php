20dc8b774e332353fb9cabcc02ce20a1d8e0f7cb
Brandon Orion Ferrell <boferre@webapp.cs.clemson.edu>
2019-02-21 00:59:20 -0500

    Added a base media preview. Changed footer css so it always sticks to the bottom of the page.

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
+<!DOCTYPE html>
+
 <head>
 	<link rel="stylesheet" type="text/css" href="css/template.css" />
+	<link rel="stylesheet" type="text/css" href="css/slider.css" />
 	<title>MeTube - Home</title>
 	<link rel="icon" href="" />
 	<meta charset="UTF-8">
@@ -10,11 +13,11 @@
 
 
 <html id="webPage">
-
+<div id="container">
 	<header id="headCont">
 		<div id="headData">
 			<span id="logoCont"><a href="index.php">logo</a></span>
-			<span id="searchCont"> <input type="text" name="searchVal"><button type="button">Search</button></span>  
+			<span id="searchCont"> <input type="text" id="searchBar"  name="searchVal"><button type="button">Search</button></span>  
 			<a id="menuCont" href="register.php">Register</a> <a id="menuCont" href="upload.php">Upload</a>
 		</div>
 	</header>
@@ -24,12 +27,21 @@
 			<tr id="spotlightedVid">
 				<td>
 					spotlighted Media
+					
 				</td>
 			</tr>
 			
 			<tr id="mediaPrev">
 				<td>
-					Media preview section
+		
+					<div class="carousel js-carousel">
+						<div class="item">test</div>
+						<div class="item">test</div>
+						<div class="item">test</div>
+						<div class="item">test</div>
+						<div class="item">test</div>
+					</div>
+					
 				</td>
 			</tr>
 
@@ -44,4 +56,5 @@
 	<footer id="footCont">
 		other links and information
 	</footer>
+</div>
 </html>
