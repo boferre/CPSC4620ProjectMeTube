97f3dc2a19d7bfc2beb26f0e77b3b514be179d46
Brandon Orion Ferrell <boferre@webapp.cs.clemson.edu>
2019-02-28 10:42:44 -0500

    Registration working still needs error checking

--- functions/registerFunction.php
+++ functions/registerFunction.php
@@ -2,28 +2,60 @@
 
 <?php
 
-// Always include this file in order to connect to database
-include 'server.php';
-
-// Needed variables;
-$accountID = 1;
-$username = 'admin';
-$displayname = 'test';
-$Fname = 'test';
-$Lname = 'test';
-$secquestion = 1;
-$secanswer = 'test';
-
-//Check if account exist
-
-// Insert into
-$sql = "INSERT INTO account VALUES ('$accountID', '$username', '$displayname', '$Fname', '$Lname', '$secquestion', '$secanswer');";
+function check() {
+	
+	return false;
+}
 
 
-//echo $sql;	
+function accountCheck($value) {
+	include 'server.php';
+	$sql = "SELECT username FROM account WHERE username='$value';";
+	echo $conn->query($sql);
+	
+	
+	return false;
+}
 
-$conn->query($sql);
 
-// Always close the connection
-$conn->close();
-?> 
+function hashuser() {
+	
+}
+
+
+function register() {
+	// Always include this file in order to connect to database
+	include 'server.php';
+	
+	// Get required variables;
+	// Determine Account ID
+	$accountID = 1;
+	$username = mysqli_real_escape_string($conn, $_REQUEST['email']);
+	$displayname = mysqli_real_escape_string($conn, $_REQUEST['dName']);
+	$Fname = mysqli_real_escape_string($conn, $_REQUEST['fName']);
+	$Lname = mysqli_real_escape_string($conn, $_REQUEST['lName']);
+	$secquestion = mysqli_real_escape_string($conn, $_REQUEST['secQuestion']);
+	$secanswer = mysqli_real_escape_string($conn, $_REQUEST['secAnswer']);
+
+	//Check if account exist
+	if (accountCheck($username)) {
+		echo "Account already exist!";
+		return;
+	}
+	// Error checking
+	if (check()) {
+		
+		echo "Account creation failed!";
+		return;
+	}
+
+	// Insert into
+	$sql = "INSERT INTO account VALUES ('$accountID', '$username', '$displayname', '$Fname', '$Lname', '$secquestion', '$secanswer');";
+	//echo $sql;	
+	$conn->query($sql);
+	// Always close the connection
+	$conn->close();
+	
+	echo "Your account has been created!";
+}
+?>
--- register.php
+++ register.php
@@ -12,6 +12,8 @@
 </head>
 
 <?php
+	include 'functions/registerFunction.php';
+
 	$accountID = 0;
 	$username = '';
 	$displayname = '';
@@ -36,19 +38,37 @@
 		<div class="regTitle" id="titleCont">Fill out the following information to register!</div>
 		<div id="infoCont">
 		
-			<div class="regCont" id="fullNameCont"><span class="heading" id="nameHead">Full Name</span> <input type="text" id="nameInput" name="fName"> <input type="text" id="nameInput" name="lName"></div>
-			<div class="regCont" id="emailCont"><span class="heading" id="emailHead">Email</span> <input type="text" id="emailInput" name="email"></div>
-			<div class="regCont" id="accName"><span class="heading" id="displayHead">Display Name</span> <input type="text" id="nameInput" name="dName"></div>
-			<div class="regCont" id="passCont"><span class="heading" id="passHead">Password</span> <input type="text" id="passInput" name="password"> <input type="text" id="passInput" name="passwordComp"></div>
+		<form method="post">
+			<div class="regCont" id="fullNameCont"><span class="heading" id="nameHead">Full Name</span> 
+				<input type="text" id="nameInput" name="fName" placeholder="Enter First Name"> 
+				<input type="text" id="nameInput" name="lName" placeholder="Enter Last Name"></div>
+			<div class="regCont" id="emailCont"><span class="heading" id="emailHead">Email</span> 
+				<input type="text" id="emailInput" name="email" placeholder="Enter Email"></div>
+			<div class="regCont" id="accName"><span class="heading" id="displayHead">Display Name</span> 
+				<input type="text" id="nameInput" name="dName" placeholder="Enter Display Name"></div>
+			<div class="regCont" id="passCont"><span class="heading" id="passHead">Password</span> 
+				<input type="text" id="passInput" name="password" placeholder="Enter Password"> 
+				<input type="text" id="passInput" name="passwordComp" placeholder="Confirm Password"></div>
 			<div class="regCont" id="secQueCont"><span class="heading" id="secHead">Security Question</span> 
-				<select>
-				  <option value="1">Question 1</option>
-				  <option value="2">Question 2</option>
-				  <option value="3">Question 3</option>
-				  <option value="4">Question 4</option>
+				<select name="secQuestion">
+				  <option value="1">What was the name of your first pet?</option>
+				  <option value="2">What was the name of the steet you lived on?</option>
+				  <option value="3">What was the name of your best friend?</option>
+				  <option value="4">What was the name of your high school?</option>
 				</select> <input type="text" id="secAnInput" name="secAnswer"> </div>
 			<div class="regCont" id="termsCont"><span class="heading" id="termsHead">Terms and Conditions</span> <input type="checkbox" name="confirmedTerms" value="yes"> I have read the terms and conditions </div>
-			<div class="regCont" id="submitCont"><button type="button" id="submitButton">Submit</button></div>
+			<div class="regCont" id="submitCont">
+				<input type="submit" name="submit" value="Submit" id="submitButton">
+			</div>
+		</form>
+		
+		<?php
+			if (isset($_POST['submit'])) {
+				register();
+				// Reset Post
+				$_POST = array();
+			}
+		?>
 		
 		</div>
 	</div>
